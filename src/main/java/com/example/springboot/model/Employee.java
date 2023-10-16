ddf5b158774e998fbd0fd38ddefe12285cd9a16f
Yerkebulan Yessenali <yerkebulanyessenali03@gmail.com>
2023-10-16 09:12:51 +0600

    create jpa entity

---
+++ src/main/java/com/example/springboot/model/Employee.java
@@ -0,0 +1,25 @@
+package com.example.springboot.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+@Table(name="employees")
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(name="first_name", nullable = false)
+    private String firstName;
+    @Column(name="last_name", nullable = false)
+    private String lastName;
+    @Column(nullable = false)
+    private String email;
+}
