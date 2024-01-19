cdde52e15f85edfb933966ff903880b30def0394
yyessenali <yyessenali@greet-go.com>
2024-01-19 12:57:46 +0600

    add unit test for getAllEmployees

--- src/test/java/com/example/springboot/service/EmployeeServiceTests.java
+++ src/test/java/com/example/springboot/service/EmployeeServiceTests.java
@@ -20,6 +20,7 @@ import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.List;
 import java.util.Optional;
 
 @ExtendWith(MockitoExtension.class)
@@ -72,7 +73,28 @@ public class EmployeeServiceTests {
             employeeService.saveEmployee(employee);
         });
 
-        //then
+        //then - verify the output
         verify(employeeRepository, never()).save(any(Employee.class));
+    }
+
+    //Junit test for getAllEmployees method
+    @DisplayName("Junit test for getAllEmployees method")
+    @Test
+    public void givenEmployeesList_whenGetAllEmployees_thenReturnEmployeesList(){
+        //given - precondition or setup
+
+        Employee employee1 = Employee.builder()
+                .id(2L)
+                .firstName("Tony")
+                .lastName("Stark")
+                .email("stark@gmail.com")
+                .build();
+
+        given(employeeRepository.findAll()).willReturn(List.of(employee, employee1));
+        //when - action or the behavior that we are going test
+        List<Employee> employeeList = employeeService.getAllEmployees();
+        //then - verify the output
+        Assertions.assertThat(employeeList).isNotNull();
+        Assertions.assertThat(employeeList.size()).isEqualTo(2);
     }
 }
